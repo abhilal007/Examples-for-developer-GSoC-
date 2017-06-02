db536b6062b670046427e23bfccbdead16349ef6
abhilal007 <abhilal007@gmail.com>
2017-06-03 00:51:05 +0530

    sample test sekl

---
+++ entity_example/src/Tests/EntityExampleTest.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @file
+ * Tests for entity_example module.
+ *
+ * Verify example module functionality.
+ */
+ namespace Drupal\entity_example\Tests;
+
+ use Drupal\simpletest\WebTestBase;
+/**
+ * Functionality tests for entity example module.
+ *
+ * @ingroup entity_example
+ */
+class EntityExampleTest extends WebTestBase{
+
+
+  
+}
